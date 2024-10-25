e7284740a195e5d2d5bc3c4ba053c0d100171720
wighawag <wighawag@gmail.com>
2024-10-25 17:23:36 +0100

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# tlock-js bug in test with cloudflare/vitest
+
+reproduction:
+
+```bash
+pnpm i
+pnpm test
+```
---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+
+export default defineWorkersConfig(async () => {
+  return {
+    test: {
+      poolOptions: {
+        workers: {
+          wrangler: {
+            configPath: "./wrangler.toml",
+          },
+        },
+      },
+    },
+  };
+});
